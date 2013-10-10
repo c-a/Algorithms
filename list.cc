9df0f69d9cc8cef2bc02d08380e42fa1d523ce32
Carl-Anton Ingmarsson <mail@carlanton.se>
2013-10-10 23:58:15 +0200

    list: Move implementation into .cc file

--- list.cc
+++ list.cc
@@ -21,6 +21,198 @@
 
 #include "list.h"
 
+namespace cai
+{
+
+template<typename ValueType>
+struct List<ValueType>::Node
+{
+  Node(const value_type& v, Node* n, Node* p) : value(v), next(n), prev(p) {};
+
+  value_type value;
+  Node* next;
+  Node* prev;
+};
+
+template<typename ValueType>
+void
+List<ValueType>::prepend(const value_type& value) {
+  Node* n = new Node(value, start_, 0);
+  if (start_ != 0)
+    start_->prev = n;
+  else
+    end_ = n;
+
+  start_ = n;
+}
+
+template<typename ValueType>
+void
+List<ValueType>::append(const value_type& value) {
+  Node* n = new Node(value, 0, end_);
+  if (end_ != 0)
+    end_->next = n;
+  else
+    start_ = n;
+
+  end_ = n;
+}
+
+template<typename ValueType>
+typename List<ValueType>::iterator
+List<ValueType>::insert(iterator position, const value_type& value) {
+  if (position.node_ == 0) {
+    append(value);
+    position.node_ = end_;
+  }
+  else if (position.node_->prev == 0) {
+    prepend(value);
+    position.node_ = end_;
+  }
+  else {
+    Node *n = new Node(value, position.node_, position.node_->prev);
+
+    position.node_->prev->next = n;
+    position.node_->prev = n;
+    position.node_ = n;
+  }
+
+  return position;
+}
+
+template<typename ValueType>
+typename List<ValueType>::Node*
+List<ValueType>::findMiddle(Node* n) {
+  Node *slow, *fast;
+
+  slow = fast = n;
+  while (fast->next) { 
+    slow = slow->next;
+    if (fast->next->next == 0) {
+      break;
+    }
+    fast = fast->next->next;
+  }
+
+  return slow;
+}
+
+template<typename ValueType>
+typename List<ValueType>::Node* List<ValueType>::merge(Node* l1, Node* l2) {
+  Node *head, *tail;
+
+  #define APPEND(l) do { \
+    tail->next = l; \
+    l->prev = tail; \
+    tail = l; \
+    l = l->next; \
+  } while (0)
+
+  if (l1->value <= l2->value) {
+    head = l1;
+    tail = l1;
+    l1 = l1->next;
+  }
+  else {
+    head = l2;
+    tail = l2;
+    l2 = l2->next;
+  }
+
+  while (l1 != 0 || l2 != 0) {
+    if (l1 != 0 && l2 != 0) {
+      if (l1->value <= l2->value)
+        APPEND(l1);
+      else
+        APPEND(l2);
+    }
+    else if (l1 != 0)
+      APPEND(l1);
+    else
+      APPEND(l2);
+  }
+
+  head->prev = 0;
+  tail->next = 0;
+
+  return head;
+}
+
+template<typename ValueType>
+typename List<ValueType>::Node*
+List<ValueType>::sort(Node* list) {
+  if (!list)
+    return 0;
+
+  if (list->next == 0)
+    return list;
+
+  Node* middle = findMiddle(list);
+  middle->prev->next = 0;
+
+  return(merge(sort(list), sort(middle)));
+}
+
+template<typename ValueType>
+class List<ValueType>::ListIterator :
+  std::iterator<std::forward_iterator_tag, ValueType>
+{
+public:
+    ListIterator() : node_(0) {};
+
+  ValueType& operator*() {
+    return node_->value;
+  }
+
+  bool operator!=(const ListIterator& rhs) const {
+    return node_ != rhs.node_;
+  }
+
+  ListIterator& operator++() {
+    node_ = node_->next;
+    return *this;
+  }
+
+private:
+  friend class List;
+
+  Node* node_;
+
+  ListIterator(Node* const node) : node_(node) {};
+};
+
+template<typename ValueType>
+class List<ValueType>::ConstListIterator :
+  std::iterator<std::forward_iterator_tag, ValueType>
+{
+public:
+  ConstListIterator() : node_(0) {};
+
+  ConstListIterator(const ListIterator& iter) : node_(iter.node_) {};
+
+  const ValueType& operator*() const {
+    return node_->value;
+  }
+
+  bool operator!=(const ConstListIterator& rhs) const {
+    return node_ != rhs.node_;
+  }
+
+  ConstListIterator& operator++() {
+    node_ = node_->next;
+    return *this;
+  }
+
+private:
+  friend class List;
+
+  const Node* node_;
+
+  ConstListIterator(Node* const node) : node_(node) {};
+};
+
+};
+
 int main(int argc, char* argv[])
 {
   int i;
@@ -28,7 +220,7 @@ int main(int argc, char* argv[])
   cai::List<int>::iterator iter;
   cai::List<int>::const_iterator citer;
 
-  for (i = 0; i < 10000; i++)
+  for (i = 0; i < 10; i++)
     l.append(i);
 
 #if 0
--- list.h
+++ list.h
@@ -28,8 +28,8 @@ namespace cai
 template<typename ValueType>
 class List
 {
+
 private:
-  struct Node;
   class ListIterator;
   class ConstListIterator;
 
@@ -56,45 +56,10 @@ public:
     return const_iterator(0);
   }
 
-  void prepend(const value_type& value) {
-    Node* n = new Node(value, start_, 0);
-    if (start_ != 0)
-      start_->prev = n;
-    else
-      end_ = n;
+  void prepend(const value_type& value);
+  void append(const value_type& value);
 
-    start_ = n;
-  }
-
-  void append(const value_type& value) {
-    Node* n = new Node(value, 0, end_);
-    if (end_ != 0)
-      end_->next = n;
-    else
-      start_ = n;
-
-    end_ = n;
-  }
-
-  iterator insert(iterator position, const value_type& value) {
-    if (position.node_ == 0) {
-      append(value);
-      position.node_ = end_;
-    }
-    else if (position.node_->prev == 0) {
-      prepend(value);
-      position.node_ = end_;
-    }
-    else {
-      Node *n = new Node(value, position.node_, position.node_->prev);
-
-      position.node_->prev->next = n;
-      position.node_->prev = n;
-      position.node_ = n;
-    }
-
-    return position;
-  }
+  iterator insert(iterator position, const value_type& value);
 
   void sort() {
     start_ = sort(start_);
@@ -103,143 +68,13 @@ public:
 protected:
 
 private:
+  struct Node;
 
-  static Node* sort(Node* list) {
-    if (!list)
-      return 0;
-
-    if (list->next == 0)
-      return list;
-
-    Node* middle = findMiddle(list);
-    middle->prev->next = 0;
-
-    return(merge(sort(list), sort(middle)));
-  }
-
-  static Node* findMiddle(Node* n) {
-    Node *slow, *fast;
-
-    slow = fast = n;
-    while (fast->next) { 
-      slow = slow->next;
-      if (fast->next->next == 0) {
-        break;
-      }
-      fast = fast->next->next;
-    }
-
-    return slow;
-  }
-
-  static Node* merge(Node* l1, Node* l2) {
-    Node *head, *tail;
-
-    #define APPEND(l) do { \
-      tail->next = l; \
-      l->prev = tail; \
-      tail = l; \
-      l = l->next; \
-    } while (0)
-
-    if (l1->value <= l2->value) {
-      head = l1;
-      tail = l1;
-      l1 = l1->next;
-    }
-    else {
-      head = l2;
-      tail = l2;
-      l2 = l2->next;
-    }
-
-    while (l1 != 0 || l2 != 0) {
-      if (l1 != 0 && l2 != 0) {
-        if (l1->value <= l2->value)
-          APPEND(l1);
-        else
-          APPEND(l2);
-      }
-      else if (l1 != 0)
-        APPEND(l1);
-      else
-        APPEND(l2);
-    }
-
-    head->prev = 0;
-    tail->next = 0;
-
-    return head;
-  }
-
-  struct Node
-  {
-    Node(const value_type& v, Node* n, Node* p) : value(v), next(n), prev(p) {};
-
-    value_type value;
-    Node* next;
-    Node* prev;
-  };
-
-  class ListIterator :
-    std::iterator<std::forward_iterator_tag, ValueType>
-  {
-    public:
-      ListIterator() : node_(0) {};
-
-      ValueType& operator*() {
-        return node_->value;
-      }
-
-      bool operator!=(const ListIterator& rhs) const {
-        return node_ != rhs.node_;
-      }
-
-      ListIterator& operator++() {
-        node_ = node_->next;
-        return *this;
-      }
-
-    private:
-      friend class List;
-
-      Node* node_;
-
-      ListIterator(Node* const node) : node_(node) {};
-  };
-
-  class ConstListIterator :
-    std::iterator<std::forward_iterator_tag, ValueType>
-  {
-    public:
-      ConstListIterator() : node_(0) {};
-
-      ConstListIterator(const ListIterator& iter) : node_(iter.node_) {};
-
-      const ValueType& operator*() const {
-        return node_->value;
-      }
-
-      bool operator!=(const ConstListIterator& rhs) const {
-        return node_ != rhs.node_;
-      }
-
-      ConstListIterator& operator++() {
-        node_ = node_->next;
-        return *this;
-      }
-
-    private:
-      friend class List;
-
-      const Node* node_;
-
-      ConstListIterator(Node* const node) : node_(node) {};
-  };
-
-  friend ListIterator;
-  friend ConstListIterator;
+  static Node* sort(Node* list);
+  static Node* findMiddle(Node* n);
   
+  static Node* merge(Node* l1, Node* l2);
+
   Node* start_;
   Node* end_;
 };
